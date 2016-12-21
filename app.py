42ee8e14c98d6e225701048976fd53c8548be50c
Edvardas Dlugauskas <edvardasdlugauskas@gmail.com>
2016-12-21 16:27:28 +0200

    Fixed the formula font, now appears even w/o entering values. "Answer" does not appear before submitting.

--- app.py
+++ app.py
@@ -23,16 +23,19 @@ def application():
     if request.method == "POST":
         try:
             expression = request.form["expression"]
-            all_symb, delta_symb, non_delta_symb = all_delta_nondelta(expression)
-            derivatives = partial_derivatives(expression, non_delta_symb)
-        except Exception as e:
-            error = "Could not understand formula (did you forget a '*'?)"
-            print(e)
+            if expression.strip() == "":
+                error = "Please enter a formula"
+            else:
+                all_symb, delta_symb, non_delta_symb = all_delta_nondelta(expression)
+                derivatives = partial_derivatives(expression, non_delta_symb)
+                with_deltas = indirect_observation_error(derivatives, delta_symb)
+                pretty = sympy.printing.pretty(with_deltas, use_unicode=True)
+        except (SyntaxError, sympy.SympifyError) as e:
+            error = "Could not understand formula"
+            print("Error: " + str(e))
 
         try:
             subs = {smb: request.form[str(smb)] for smb in all_symb}
-            with_deltas = indirect_observation_error(derivatives, delta_symb)
-            pretty = sympy.printing.pretty(with_deltas, use_unicode=True)
             answer = with_deltas.subs(subs).evalf()
         except Exception as e:
             print(e)
